a1307c368559e00356d7ef2de2cd321faa9df047
Mohamed Idbellaali <mohidbel@e3r9p18.1337.ma>
2026-03-02 22:44:32 +0000

    one eazy i'm taired today

---
+++ LeetCode/Eazy/XOR Operation in an Array.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/xor-operation-in-an-array/
+
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        int xorOperation(int n, int start) {
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                res ^= (start + 2 * i);
+            }
+            return res;
+        }
+};
