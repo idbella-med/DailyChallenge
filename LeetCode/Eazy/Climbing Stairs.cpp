effe77e2c79773d7df7dc7028eebf51b1bc4cda8
Mohamed Idbellaali <idbellaalim@gmail.com>
2026-03-10 23:23:03 +0000

    still ramadan

---
+++ LeetCode/Eazy/Climbing Stairs.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+
+        int a = 1, b = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+};
